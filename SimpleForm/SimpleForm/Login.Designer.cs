c34e708637ae99d566b57782b7f1b90c072319c1
Niamke Dozier <nidozie@siue.edu>
2018-05-15 21:01:11 -0500

    Setup of Username and password textboxes.

--- SimpleForm/SimpleForm/Login.Designer.cs
+++ SimpleForm/SimpleForm/Login.Designer.cs
@@ -28,24 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.LoginBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox3
+            // Username
             // 
-            this.textBox3.Location = new System.Drawing.Point(366, 297);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(213, 20);
-            this.textBox3.TabIndex = 2;
+            this.Username.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.Location = new System.Drawing.Point(390, 297);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(213, 31);
+            this.Username.TabIndex = 2;
+            this.Username.Text = "Username";
+            this.Username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(366, 370);
+            this.textBox4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(390, 370);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(213, 20);
+            this.textBox4.Size = new System.Drawing.Size(213, 31);
             this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "Password";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // LoginBtn
             // 
@@ -56,15 +65,26 @@
             this.LoginBtn.Text = "Login";
             this.LoginBtn.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(602, 111);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Car Finder";
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Firebrick;
             this.ClientSize = new System.Drawing.Size(969, 663);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.LoginBtn);
             this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Username);
             this.Name = "Login";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -73,9 +93,10 @@
         }
 
         #endregion
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox Username;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button LoginBtn;
+        private System.Windows.Forms.Label label1;
     }
 }
 
